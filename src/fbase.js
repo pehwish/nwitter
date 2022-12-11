a039b44bb89c7c98a3dd658c899aaba88ec1f19f
eunhye <pehwish@gmail.com>
2022-12-11 22:56:50 +0900

    #3 NWEETING

--- src/fbase.js
+++ src/fbase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
+import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,3 +19,4 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
