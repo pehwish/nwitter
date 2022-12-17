83815b2b049a879d2786ce93ed3b922167804861
eunhye <pehwish@gmail.com>
2022-12-17 17:49:15 +0900

    #6 FINISHING UP

--- src/fbase.js
+++ src/fbase.js
@@ -3,6 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,3 +21,4 @@ export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
 export const dbService = firebase.firestore();
+export const storageService = firebase.storage();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,9 @@
 import Nweet from 'components/Nweet';
+import NweetFactory from 'components/NweetFactory';
 import { dbService } from 'fbase';
 import React, { useEffect, useState } from 'react';
 
 const Home = ({ userObj }) => {
-  const [nweet, setNweet] = useState('');
   const [nweets, setNweets] = useState([]);
 
   useEffect(() => {
@@ -17,33 +17,10 @@ const Home = ({ userObj }) => {
     });
   }, []);
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    await dbService.collection('nweets').add({
-      text: nweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    });
-    setNweet('');
-  };
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setNweet(value);
-  };
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={nweet}
-          onChange={onChange}
-          type='text'
-          placeholder="What's on your mind?"
-          maxLength={120}
-        />
-        <input type='submit' value='Nweet' />
-      </form>
+      <NweetFactory userObj={userObj} />
+
       <div>
         {nweets.map((nweet) => (
           <Nweet
