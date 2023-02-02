e82fe57ccd4b6cee111ac5de74c14faa2166a849
박은혜 <pehwish@gmail.com>
2023-02-03 08:38:22 +0900

    Updates

---
+++ precache-manifest.26cb7e525352ae4641ef126ce7ac1e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a0b76db94aa947bbb3ac89d6aa657e",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "f2b5bf45de9e92f3f372",
+    "url": "/nwitter/static/css/main.531bf67f.chunk.css"
+  },
+  {
+    "revision": "87a5613b4a61646d52fd",
+    "url": "/nwitter/static/js/2.c7b5f8f3.chunk.js"
+  },
+  {
+    "revision": "aa85e07c1fd171ce83e42e25e36158a4",
+    "url": "/nwitter/static/js/2.c7b5f8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b5bf45de9e92f3f372",
+    "url": "/nwitter/static/js/main.bfbefbe3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/nwitter/static/js/main.bfbefbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125d1d768f4e9614d81a",
+    "url": "/nwitter/static/js/runtime-main.3a662223.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nwitter/precache-manifest.0053e8e8e19b409e011b605141d64626.js"
+  "/nwitter/precache-manifest.26cb7e525352ae4641ef126ce7ac1e90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nwitter/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
